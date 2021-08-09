0a26fd91457c7e72df6c0763b9cdc6e3b68798a7
maoshen5478 <361193645@qq.com>
2021-08-09 19:12:22 +0800

    permission menu

---
+++ src/components/admin/PermissionMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    dsad
+  </div>
+</template>
+
+<script>
+import { defaultCompent } from 'vue';
+export default defaultCompent({
+  name: 'Menu',
+  setup() {
+    return { };
+  }
+});
+</script>
+
+<style>
+
+</style>
