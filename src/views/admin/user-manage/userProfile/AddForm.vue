eaf2b26a978cb71ec09e9b4a892f06619f93ce3c
maoshen5478 <361193645@qq.com>
2021-09-07 17:36:05 +0800

    user profile ui

---
+++ src/views/admin/user-manage/userProfile/AddForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    add
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: '',
+  setup() {
+    return { };
+  }
+});
+</script>
+
+<style>
+
+</style>
